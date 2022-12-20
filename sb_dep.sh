127a2402a3b2802e8a353c1d29390db2ec8dab98
salty <salty@salty.dk>
2022-12-20 19:23:32 +0100

    Bump Ansible to 7.x

--- sb_dep.sh
+++ sb_dep.sh
@@ -34,7 +34,7 @@ readonly PYTHON_CMD_SUFFIX="-m pip install \
                               --disable-pip-version-check \
                               --upgrade"
 readonly PYTHON3_CMD="/srv/ansible/venv/bin/python3 $PYTHON_CMD_SUFFIX"
-readonly ANSIBLE=">=6.0.0,<7.0.0"
+readonly ANSIBLE=">=7.0.0,<8.0.0"
 
 ################################
 # Argument Parser
