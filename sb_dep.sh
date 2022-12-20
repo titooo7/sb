e488dbf9878fb617841d9bba876c2c43bd84de1f
salty <salty@salty.dk>
2022-12-20 18:46:20 +0100

    remove root warning flag

--- sb_dep.sh
+++ sb_dep.sh
@@ -32,8 +32,7 @@ readonly PYTHON_CMD_SUFFIX="-m pip install \
                               --timeout=360 \
                               --no-cache-dir \
                               --disable-pip-version-check \
-                              --upgrade \
-                              --root-user-action=ignore"
+                              --upgrade"
 readonly PYTHON3_CMD="/srv/ansible/venv/bin/python3 $PYTHON_CMD_SUFFIX"
 readonly ANSIBLE=">=6.0.0,<7.0.0"
 
