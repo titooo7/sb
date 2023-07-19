7408dcb26d3d3efdef2847593fa74daf3c6b389a
titooo7 <juanmartinbravo@gmail.com>
2023-07-19 16:58:09 +0200

    Update sb_install.sh
    my repo

--- sb_install.sh
+++ sb_install.sh
@@ -15,7 +15,7 @@
 
 VERBOSE=false
 VERBOSE_OPT=""
-SB_REPO="https://github.com/saltyorg/sb.git"
+SB_REPO="https://github.com/titooo7/sb.git"
 SB_PATH="/srv/git/sb"
 SB_INSTALL_SCRIPT="$SB_PATH/sb_install.sh"
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
